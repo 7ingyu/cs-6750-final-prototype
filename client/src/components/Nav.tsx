6606d42e2b3ed3bc4b751f894b8f134ddddfb7b9
Ting-Yu Chen <7ingyu@gmail.com>
2024-12-04 00:08:34 -0800

    removed unused nav button

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
 import { Link, useLocation } from "react-router";
+// import { Offcanvas } from "react-bootstrap";
 import { HistoryContext } from "@/context";
 
 const Nav = () => {
-  const handleClick = () => {};
   const { pathname } = useLocation();
   const [history, setHistory] = useContext(HistoryContext);
+  // const [show, setShow] = useState(false);
 
   return (
     <nav className="">
@@ -21,13 +22,22 @@ const Nav = () => {
             <span>{history[history.length - 2].title}</span>
           </Link>
         ) : null}
-        <button
+        {/* <button
           className="ms-auto btn btn-link btn-lg p-1"
           onClick={handleClick}
         >
           <i className="bi bi-list"></i>
-        </button>
+        </button> */}
       </div>
+      {/* <Offcanvas show={show} onHide={() => setShow(false)}>
+        <Offcanvas.Body>
+          <ul className="list-unstyled">
+            <li>
+              Export Tag
+            </li>
+          </ul>
+        </Offcanvas.Body>
+      </Offcanvas> */}
     </nav>
   );
 };
